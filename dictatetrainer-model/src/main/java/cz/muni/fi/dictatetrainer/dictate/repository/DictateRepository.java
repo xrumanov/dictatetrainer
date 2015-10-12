ef76bb74c6adcde51c7630b838d9a8c16bb95fad
xrumanov <jakub.rumanovsky@gmail.com>
2015-10-12 10:26:11 +0200

    Correcting typo in variable user -> uploader

--- dictatetrainer-model/src/main/java/cz/muni/fi/dictatetrainer/dictate/repository/DictateRepository.java
+++ dictatetrainer-model/src/main/java/cz/muni/fi/dictatetrainer/dictate/repository/DictateRepository.java
@@ -30,8 +30,8 @@ public class DictateRepository extends GenericRepository<Dictate> {
         final StringBuilder clause = new StringBuilder("Where e.id is not null");
         final Map<String, Object> queryParameters = new HashMap<>();
         if (dictateFilter.getUploaderId() != null) {
-            clause.append(" AND e.user.id = :userId");
-            queryParameters.put("userId", dictateFilter.getUploaderId());
+            clause.append(" AND e.uploader.id = :uploaderId");
+            queryParameters.put("uploaderId", dictateFilter.getUploaderId());
         }
         if (dictateFilter.getCategoryId() != null) {
             clause.append(" AND e.category.id = :categoryId");
